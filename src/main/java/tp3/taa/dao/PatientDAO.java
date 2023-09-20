de8576cb07124af12662b1a8977bed3e35d2d17f
léo noliere <l.noliere@gmail.com>
2023-09-20 12:19:16 +0200

    addPatient & deletePatientbyId are working fine

--- src/main/java/tp3/taa/dao/PatientDAO.java
+++ src/main/java/tp3/taa/dao/PatientDAO.java
@@ -1,8 +1,11 @@
 package tp3.taa.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import tp3.taa.business.Patient;
 
+@Repository
 public interface PatientDAO extends JpaRepository<Patient, Long> {
 
 }
--- src/main/java/tp3/taa/web/PatientResource.java
+++ src/main/java/tp3/taa/web/PatientResource.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import tp3.taa.business.MedecinDTO;
 import tp3.taa.business.Patient;
 import tp3.taa.business.PatientDTO;
 import tp3.taa.business.RDV;
@@ -45,14 +49,38 @@ public class PatientResource {
     return listDTO;
   }
 
-  @RequestMapping("/addPatient")
+  @PostMapping("/addPatient")
   @ResponseBody
-  public String addPatient(Patient patient) {
+  public String addPatient(@RequestBody MedecinDTO med ) {
+    String patientId = "";
+    try {
+      Patient p = new Patient(med.getName());
+      Pdao.save(p);
+      patientId = String.valueOf(p.getId());
+    }
+    catch (Exception ex) {
+      return "Error creating the user: " + ex.toString();
+    }
+   
 
-    Pdao.save(patient);
-
-    return "User succesfully created with id = " + patient.getId();
+    return "User succesfully created with id = " + patientId;
   }
+
+  @GetMapping("/delete/{patientId}")
+	@ResponseBody
+	public String deletePatientById(@PathVariable("patientId") Long patientId) {
+    String name = Pdao.getReferenceById(patientId).getName();
+		try {
+
+			Pdao.deleteById(patientId);
+
+		} catch (Exception ex) {
+
+			return "Error deleting the user:" + ex.toString();
+
+		}
+		return "Patient" + name + "succesfully deleted!";
+	}
 
   public PatientDTO patientIntoDTO(Patient p) {
 
