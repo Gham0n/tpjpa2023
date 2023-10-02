b5b79f30c5c70f4ff17275d2f796a6af51988872
Gui <guillaumehamon.gh@gmail.com>
2023-10-02 15:40:56 +0200

    Ajout keycloak

---
+++ src/main/java/tp3/taa/web/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package tp3.taa.web;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author : Olivier Barais
+ * @created : 20-10-2023
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeHttpRequests(registry -> {
+			try {
+				registry.requestMatchers("/").permitAll()
+						.requestMatchers("/index").hasRole("user")
+						.requestMatchers("/admin").hasRole("admin")
+						.anyRequest().authenticated();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}).oauth2ResourceServer(oauth2Configurer -> oauth2Configurer
+				.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwt -> {
+					Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+					Collection<String> roles = realmAccess.get("roles");
+					List<SimpleGrantedAuthority> grantedAuthorities = roles.stream()
+							.map(role -> new SimpleGrantedAuthority("ROLE_" + role)).toList();
+					grantedAuthorities.forEach(e -> {
+						System.err.println(e.getAuthority());
+
+					});
+					return new JwtAuthenticationToken(jwt, grantedAuthorities);
+				})));
+		return httpSecurity.build();
+	}
+
+}
